c532a65339ff18cbc604b4b6cc105e38e7d4e5c9
nourd <nourd@10.10.12.63>
2024-07-31 14:20:06 +0300

    Add incomeReportpage+control and ordersreportPage and control

---
+++ BiteMe-Client/src/gui/OrdersReportControl.java
@@ -0,0 +1,35 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+public class OrdersReportControl {
+
+    @FXML
+    private TableView<?> ordersReportTableView;
+
+    @FXML
+    private Label branchnameLabel;
+
+    @FXML
+    private Label restaurantnameLabel;
+
+    @FXML
+    private Label OodersReportLabel;
+
+    @FXML
+    private Button finishOrdersReportButton;
+    
+    
+
+    @FXML
+    void finishOrdersReportButtonOnClickAction(ActionEvent event) {
+
+    }
+    
+    
+
+}
